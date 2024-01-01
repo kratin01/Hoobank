a6de2a1d59ce1845082b5cfabec22b6ebd9e5a95
kratin aggarwal <kratinaggarwal8750@gmail.com>
2024-01-01 23:59:44 +0530

    Add Animation

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({styles}) =>  (
     <button
     type='button'
-    className={`bg-blue-gradient py-4 px-4 rounded-md font-poppins text-[18px] outline-none font-medium text-primary ${styles}`}>
+    className={`bg-blue-gradient py-4 px-4 rounded-md font-poppins text-[18px] outline-none font-medium text-primary ${styles}   hover:scale-[1.3]  transition-all ease-in duration-300`}>
       Get Started
     </button>
   )
